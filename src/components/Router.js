d7c0fe81fd4cff81888ddcd592b52b6bac9184fc
choi woo seok <cwooseok60@gmail.com>
2022-07-18 03:20:51 +0900

    로그인 라우팅 스켈레톤 코드 작성

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,11 +1,23 @@
-import {HashRouter as Router, Route, Switch} from "react-router-dom";
+import {HashRouter as Router, Route, Routes} from "react-router-dom";
+import {useState} from "react";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
 
 const AppRouter = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
   return (
     <Router>
-      <Switch>
-        <Route/>
-      </Switch>
+      <Routes>{
+        isLoggedIn ? (
+            <Route path="/" element={<Home/>}/>
+          )
+          : (
+            <Route path="/" element={<Auth/>}/>
+          )
+      }
+      </Routes>
     </Router>
   )
 }
+
+export default AppRouter;
